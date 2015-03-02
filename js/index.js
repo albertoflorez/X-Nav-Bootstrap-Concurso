a8e5719e874fad9086eb06616c66e7a63e96cb3d
albertoflorez <albertoflorez4@gmail.com>
2015-03-02 23:59:13 +0100

    acabada la pagina del grupo de investigacion

---
+++ js/index.js
@@ -0,0 +1,81 @@
+
+var activeEl = 2;
+$(function() {
+
+    "use strict";
+
+    var $bgobj = $(".ha-bg-parallax"); // assigning the object
+
+    $(window).on("scroll", function () {
+
+        var yPos = -($(window).scrollTop() / $bgobj.data('speed'));
+
+        // Put together our final background position
+
+        var coords = '100% ' + yPos + 'px';
+
+        // Move the background
+
+        $bgobj.css({ backgroundPosition: coords });
+
+    });
+        $('div.product-chooser').not('.disabled').find('div.product-chooser-item').on('click', function(){
+		$(this).parent().parent().find('div.product-chooser-item').removeClass('selected');
+		$(this).addClass('selected');
+		$(this).find('input[type="radio"]').prop("checked", true);
+		
+	});
+
+
+    var items = $('.btn-nav');
+    $( items[activeEl] ).addClass('active');
+    $( ".btn-nav" ).click(function() {
+        $( items[activeEl] ).removeClass('active');
+        $( this ).addClass('active');
+        activeEl = $( ".btn-nav" ).index( this );
+    });
+});
+
+$(document).on('click', '.panel-heading span.clickable', function (e) {
+    var $this = $(this);
+    if (!$this.hasClass('panel-collapsed')) {
+        $this.parents('.panel').find('.panel-body').slideUp();
+        $this.addClass('panel-collapsed');
+        $this.find('i').removeClass('glyphicon-minus').addClass('glyphicon-plus');
+    } else {
+        $this.parents('.panel').find('.panel-body').slideDown();
+        $this.removeClass('panel-collapsed');
+        $this.find('i').removeClass('glyphicon-plus').addClass('glyphicon-minus');
+    }
+});
+$(document).on('click', '.panel div.clickable', function (e) {
+    var $this = $(this);
+    if (!$this.hasClass('panel-collapsed')) {
+        $this.parents('.panel').find('.panel-body').slideUp();
+        $this.addClass('panel-collapsed');
+        $this.find('i').removeClass('glyphicon-minus').addClass('glyphicon-plus');
+    } else {
+        $this.parents('.panel').find('.panel-body').slideDown();
+        $this.removeClass('panel-collapsed');
+        $this.find('i').removeClass('glyphicon-plus').addClass('glyphicon-minus');
+    }
+});
+$(document).ready(function () {
+    $('.panel-heading span.clickable').click();
+    $('.panel div.clickable').click();
+
+
+    $('.btn-vertical-slider').on('click', function () {
+        
+        if ($(this).attr('data-slide') == 'next') {
+            $('#myCarousel2').carousel('next');
+        }
+        if ($(this).attr('data-slide') == 'prev') {
+            $('#myCarousel2').carousel('prev')
+        }
+
+    });
+
+
+
+});
